b695cf049b5d3fa890e6fbdb777b58394ffadfb3
tubigervie <etubig@uci.edu>
2022-09-24 17:19:12 -0400

    ProductAPI  controller / CRUD operations

---
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,119 @@
+﻿using Mango.Services.ProductAPI.Models.Dto;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/products")]
+    public class ProductAPIController : ControllerBase
+    {
+
+        protected ResponseDto _response;
+        private IProductRepository _productRepository;
+
+        public ProductAPIController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+            _response = new ResponseDto();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _productRepository.GetProducts();
+                _response.Result = productDtos;
+                return Ok(_response);
+            }
+            catch(Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<String>() { ex.ToString() };
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<object> GetById(int id)
+        {
+            try
+            {
+                ProductDto productDto = await _productRepository.GetProductById(id);
+                if (productDto == null) return NotFound();
+                _response.Result = productDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<String>() { ex.ToString() };
+                return NotFound();
+            }
+            return Ok(_response);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<object> Post([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                ProductDto model = await _productRepository.CreateUpdateProduct(productDto);
+                _response.Result = model;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<String>() { ex.ToString() };
+                return BadRequest();
+            }
+            return CreatedAtAction(nameof(GetById), new { id = productDto.ProductId }, _response);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<object> Put([FromBody] ProductDto productDto) //basically same as POST but the actual logic that makes Put/post different lies within the repository
+        {
+            try
+            {
+                ProductDto model = await _productRepository.CreateUpdateProduct(productDto);
+                _response.Result = model;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<String>() { ex.ToString() };
+                return BadRequest();
+            }
+            return CreatedAtAction(nameof(GetById), new { id = productDto.ProductId }, _response);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<object> DeleteById(int id)
+        {
+            try
+            {
+                bool isSuccess = await _productRepository.DeleteProduct(id);
+                _response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<String>() { ex.ToString() };
+                return BadRequest();
+            }
+            return Ok(_response);
+        }
+
+    }
+}
--- Mango.Services.ProductAPI/DBContexts/ApplicationDbContext.cs
+++ Mango.Services.ProductAPI/DBContexts/ApplicationDbContext.cs
@@ -12,5 +12,47 @@ namespace Mango.Services.ProductAPI.DBContexts
 
         public DbSet<Product> Products { get; set; } //once this is initially added, run "add-migration AddProductModelToDb" in order to create a migration class as per specified name
                                                      //once added, run "update-database" in order to execute the last migration file ^ which applies changes to the database
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 1,
+                Name = "Samosa",
+                Price = 15,
+                Description = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ImageUrl = "https://tubigervie.blob.core.windows.net/mango/Images/11.jpg",
+                CategoryName = "Appetizer"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 2,
+                Name = "Paneer Tikka",
+                Price = 13.99,
+                Description = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ImageUrl = "https://tubigervie.blob.core.windows.net/mango/Images/12.jpg",
+                CategoryName = "Appetizer"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 3,
+                Name = "Sweet Pie",
+                Price = 10.99,
+                Description = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ImageUrl = "https://tubigervie.blob.core.windows.net/mango/Images/13.jpg",
+                CategoryName = "Dessert"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 4,
+                Name = "Pav Bhaji",
+                Price = 15,
+                Description = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ImageUrl = "https://tubigervie.blob.core.windows.net/mango/Images/14.jpg",
+                CategoryName = "Entree"
+            });
+        }
     }
 }
