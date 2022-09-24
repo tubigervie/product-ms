f4c520fac45706d48188525a4f90922981e35c31
tubigervie <etubig@uci.edu>
2022-09-24 15:58:52 -0400

    Product DTO and IProductRepository interface

---
+++ Mango.Services.ProductAPI/Models/Dto/ProductDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.ProductAPI.Models.Dto
+{
+    public class ProductDto //No annotations as you're not adding these DTOs into the database ( that's what Product is for)
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string Description { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Mango.Services.ProductAPI.Models.Dto;
+
+namespace Mango.Services.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        //What will be all the methods needed to perform CRUD operations on Products?
+        Task<IEnumerable<ProductDto>> GetProducts(); //tasks represent asynchronous operations
+        Task<ProductDto> GetProductById(int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
+    }
+}
