e7d9a4b247262e9391655408b93994bd6a728b23
tubigervie <etubig@uci.edu>
2022-09-24 14:21:04 -0400

    Initial Web API template

---
+++ Mango.Services.ProductAPI/DBContexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.DBContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) //<-- passes these options to the base class
+        {
+
+        }
+    }
+}
