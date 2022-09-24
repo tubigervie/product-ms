fe3ce2d538528172ad6d353e4ee9180e1954ff0e
tubigervie <etubig@uci.edu>
2022-09-24 15:41:51 -0400

    Configured DbContext for Product API, created Product model and created table in SQL server database

--- Mango.Services.ProductAPI/DBContexts/ApplicationDbContext.cs
+++ Mango.Services.ProductAPI/DBContexts/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Mango.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.ProductAPI.DBContexts
 {
@@ -8,5 +9,8 @@ namespace Mango.Services.ProductAPI.DBContexts
         {
 
         }
+
+        public DbSet<Product> Products { get; set; } //once this is initially added, run "add-migration AddProductModelToDb" in order to create a migration class as per specified name
+                                                     //once added, run "update-database" in order to execute the last migration file ^ which applies changes to the database
     }
 }
---
+++ Mango.Services.ProductAPI/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.ProductAPI.Models
+{
+    public class Product
+    {
+        [Key] //annotation that specifies that this is the primary key
+        public int ProductId { get; set; }
+
+        [Required] //ensures a non-nullable field in DB
+        public string Name { get; set; }
+
+        [Range(1, 1000)] //in USD
+        public double Price { get; set; }
+
+        public string Description { get; set; }
+
+        public string CategoryName { get; set; } //TODO : move this category into another table
+
+        public string ImageUrl { get; set; }
+
+    }
+}
